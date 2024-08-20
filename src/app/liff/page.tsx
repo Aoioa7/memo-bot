79ccb636dba6d383ac6970050fa47d1f06bcb479
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-08-20 22:38:54 +0900

    fixed

--- src/app/liff/page.tsx
+++ src/app/liff/page.tsx
@@ -1,20 +1,22 @@
 "use client";
+import react, { useState } from 'react';
 import { useEffect } from 'react';
 
 import Form from "./Components/Form";
 import Title from "./Components/Title";
 
 export default function Home() {
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(()=> {
+	  setMounted(true);
+	}, [])
+
 	if (typeof window === 'undefined') {
 		console.log('we are not  running on the client')
 	}
-	let flag = false
-	useEffect(()=> {
-		flag = true
-	  }, [])
-	  if (!flag) {
-		return
-	  }
+	
+	  if (mounted) {
 		return (
 			<main className="min-h-screen bg-gray-100 py-10 px-4 sm:px-6 lg:px-8">
 			  <div className="max-w-md mx-auto bg-green-100  shadow-md">
@@ -23,4 +25,5 @@ export default function Home() {
 			  </div>
 			</main>
 		  )
+		}
 }
