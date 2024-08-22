a5ad066ffb1f6ae6804742f8bf403c7dbab6fb8b
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-08-22 11:24:46 +0900

    functuons

--- src/app/api/main/route.ts
+++ src/app/api/main/route.ts
@@ -3,25 +3,25 @@ export async function POST(request: Request) {
 	//request-check
 
 	//follow
-	if (1) {
+	if (-1) {
 		//処理内容
 
 	}
 	//send
-	if (1) {
-		//AIチャットモード
+	if (0) {
+		//AIチャットモード(適当なチャット/@memo-mode)
 
 		//関数
 
 	}
 	else if (1) {
-		//メモ操作モード
+		//メモ操作モード(create タイトル/delete メモID/memo/@chat-mode)
 
 		//関数
 
 	}
-	else if (1) {
-		//プロジェクト操作モード
+	else if (2) {
+		//メモ内容登録モード(メモ内容送信)
 
 		//関数
 	}
