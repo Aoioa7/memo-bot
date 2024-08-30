89605a4fb85e8fba9c15057a324fad7ff18ee606
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-08-30 21:10:58 +0900

    pre

--- src/app/api/main/route.ts
+++ src/app/api/main/route.ts
@@ -11,16 +11,17 @@ export async function POST(request: Request) {
 	//webhook->follow
 	const req = await request.json();
 	const e = req.events[0]
-	client.replyMessage(e.replyToken, {
-		type: 'text',
-		text: e.message.text
-	  });
-	if (-1) {
+
+	if (1) {
 		//初回の処理内容(webhookのuserIDをuserInfoテーブルに登録)
+		client.replyMessage(e.replyToken, {
+			type: 'text',
+			text: e.message+"|"+e.message.text+"|"+e.message["text"]
+	});
 		
 	}
 	//webhook->send
-	else if (0) {
+	else if (1) {
 		//AIチャットモード(適当なチャット/@memo-mode)
 
 		//@memo-mode分岐
@@ -37,12 +38,12 @@ export async function POST(request: Request) {
 		//関数->creatememo/deletememo/memolist
 
 	}
-	else if (2) {
+	else if (1) {
 		//メモ内容登録モード(メモ内容)
 
 		//関数->write
 	}
-	else if(3) {
+	else if(1) {
 		//リマインド登録モード(何日後にリマインドするか)、
 		//ここまでのinsert(memoID,userID)->update(content)->update(remindCount)の流れで3チャットひとまとまり
 
