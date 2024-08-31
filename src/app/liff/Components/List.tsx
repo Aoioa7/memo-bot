c9a650d3c84ed24458e60c8fa74f1f17c8debda7
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-09-01 02:51:52 +0900

    fixed

--- src/app/liff/Components/List.tsx
+++ src/app/liff/Components/List.tsx
@@ -16,16 +16,20 @@ interface ListProps {
 export default function List({ memoList, onDelete }: ListProps) {
 	let memoList2:string[] =[]
 	for (const memo of memoList) {
-		let pre_memo2=marked.parse(memo.replace(/( )/g,'&nbsp;').replace(/(　)/g,'&emsp;').replace(/(\n)/g,'<br>'))
+		let pre_memo2=marked.parse(memo.replace(/(　)/g,'&nbsp;'))
 		if (!(pre_memo2 instanceof Promise)) {
 			pre_memo2=pre_memo2.replace(/(<ul>)|(<\/ul>)/g,'')
-			memoList2.push('<ul style="list-style: disc; padding-left:13px">'+pre_memo2+'</ul>')}
+			memoList2.push('<ul style="list-style: disc; padding-left:13px;">'+pre_memo2.replace(/(\n)/g,'<br/>')+'</ul>')}
 	}
     return (
         <ul className="mt-6 space-y-4">
         {memoList2.map((memo2: string, index: number) => (
             <li key={index} className="bg-white border border-gray-300 px-4 py-2">
-			<span id="output" className="break-words ">{parse(memo2)}</span>
+			<span id="output" className="break-words ">{} 
+				{parse(memo2.replace(/(<ul style="list-style: disc; padding-left:13px;"><br\/>)/,'<ul style="list-style: disc; padding-left:13px;">')
+				.replace(/(<br\/><\/ul>)/,'</ul>')
+				.replace(/(<br\/><\/li>)/g,'</li>').replace(/(<\/li><br\/>)/g,'</li>')
+				.replace(/(<br\/><li>)/g,'<li>').replace(/(<li><br\/>)/g,'<li>'))}</span>
             <p><button className="bg-red-600 text-white px-4 py-1 rounded-md" onClick={() => onDelete(index)}>delete</button></p>
             </li>
         ))}
