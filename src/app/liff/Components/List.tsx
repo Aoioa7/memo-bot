fe94aa74fa00e49c3ab93c2c25e69a87329f9b91
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-09-01 05:08:18 +0900

    fixed

--- src/app/liff/Components/List.tsx
+++ src/app/liff/Components/List.tsx
@@ -19,6 +19,7 @@ export default function List({ memoList, onDelete }: ListProps) {
 		let pre_memo2=marked.parse(memo.replace(/(　)/g,'&nbsp;'))
 		if (!(pre_memo2 instanceof Promise)) {
 			pre_memo2=pre_memo2.replace(/(<ul>)|(<\/ul>)/g,'')
+			.replace(/(<a href=".+">.+<\/a>)/g,'<strong><font color=”blue”>$1</font></strong>')
 			memoList2.push('<ul style="list-style: disc; padding-left:13px;">'+pre_memo2.replace(/(\n)/g,'<br/>')+'</ul>')}
 	}
     return (
@@ -26,8 +27,8 @@ export default function List({ memoList, onDelete }: ListProps) {
         {memoList2.map((memo2: string, index: number) => (
             <li key={index} className="bg-white border border-gray-300 px-4 py-2">
 			<span id="output" className="break-words ">{} 
-				{parse(memo2.replace(/(<ul style="list-style: disc; padding-left:13px;"><br\/>)/,'<ul style="list-style: disc; padding-left:13px;">')
-				.replace(/(<br\/><\/ul>)/,'</ul>')
+				{parse(memo2.replace(/(<ul style="list-style: disc; padding-left:13px;"><br\/>)/g,'<ul style="list-style: disc; padding-left:13px;">')
+				.replace(/(<br\/><\/ul>)/g,'</ul>')
 				.replace(/(<br\/><\/li>)/g,'</li>').replace(/(<\/li><br\/>)/g,'</li>')
 				.replace(/(<br\/><li>)/g,'<li>').replace(/(<li><br\/>)/g,'<li>'))}</span>
             <p><button className="bg-red-600 text-white px-4 py-1 rounded-md" onClick={() => onDelete(index)}>delete</button></p>
