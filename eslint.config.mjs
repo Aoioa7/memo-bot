70c3f92f2381efcfbf2e6f2de4af2d31f6a716ac
aoi <149755797+Aoioa7@users.noreply.github.com>
2024-09-09 10:59:18 +0900

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,14 +5,15 @@ import pluginReact from 'eslint-plugin-react'
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
-  { settings: {
-	react: {
-	  version: "detect",
-	},
-  }},
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  
 ]
